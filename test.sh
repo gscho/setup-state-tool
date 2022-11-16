1c683da118be55adfacfe813f7f3d0cb6396cf9d
Gregory Schofield <greg.c.schofield@gmail.com>
2022-11-16 16:09:59 -0500

    First commit
    Signed-off-by: Gregory Schofield <greg.c.schofield@gmail.com>

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+installArgs=""
+      
+        installArgs="-v lol "
+      
+        installArgs="${installArgs}-b beta"
+        echo $installArgs
+      
+# sh <(curl -q https://platform.activestate.com/dl/cli/install.sh) $installArgs
