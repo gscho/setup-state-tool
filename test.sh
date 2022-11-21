b18684e94298197ea292cb7810b1721a65d3e76b
Gregory Schofield <greg.c.schofield@gmail.com>
2022-11-21 10:45:20 -0500

    Adding back windows
    Signed-off-by: Gregory Schofield <greg.c.schofield@gmail.com>

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-installArgs=""
-      
-        installArgs="-v lol "
-      
-        installArgs="${installArgs}-b beta"
-        echo $installArgs
-      
-# sh <(curl -q https://platform.activestate.com/dl/cli/install.sh) $installArgs
